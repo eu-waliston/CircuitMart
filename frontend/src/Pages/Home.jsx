a41bfdf0182cefc1f33d749154819b79c075bbe3
Waliston <goneisgone@hotmail.com>
2023-07-21 13:21:33 -0300

    Circuit Mart >> colors update

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
 
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -11,7 +12,8 @@ import { Autoplay, Pagination } from "swiper/modules";
 import { AiFillCreditCard } from "react-icons/ai";
 import { FaHandHoldingUsd } from "react-icons/fa";
 import { MdPhoneAndroid } from "react-icons/md";
-import { RiSecurePaymentLine } from "react-icons/ri"
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -45,23 +47,29 @@ const Home = () => {
         </Swiper>
       </div>
       <div className="advantages-section">
-      <div className="advantages">
-            <RiSecurePaymentLine className="icon"/>
-            <h3>Pagamento rápido e seguro!</h3>
+        <div className="advantages">
+          <RiSecurePaymentLine className="icon" />
+          <h3>Pagamento rápido e seguro!</h3>
         </div>
         <div className="advantages">
-            <AiFillCreditCard className="icon"/>
-            <h3>Até 10 parcelas sem juros!</h3>
+          <AiFillCreditCard className="icon" />
+          <h3>Até 10 parcelas sem juros!</h3>
         </div>
         <div className="advantages">
-            < FaHandHoldingUsd className="icon"/>
-            <h3>Parcelamento sem cartão</h3>
+          <FaHandHoldingUsd className="icon" />
+          <h3>Parcelamento sem cartão</h3>
         </div>
         <div className="advantages">
-            < MdPhoneAndroid className="icon"/>
-            <h3>Via Pix</h3>
+          <MdPhoneAndroid className="icon" />
+          <h3>Via Pix</h3>
         </div>
       </div>
+      <h1>Ofertas do dia</h1>
+      <div className="daily-offers-section"></div>
+      <Link to={"/thunder"}>
+      <img src="./images/relampago.png" alt="ofertas" className="ofertas" />
+      </Link>
+      <div className="daily-offers-section"></div>
     </div>
   );
 };
