cb597928008bbd65f7d8f122457b83f233f87046
Waliston <goneisgone@hotmail.com>
2023-07-22 20:50:17 -0300

    Circuit Mart, update, shop itens

--- frontend/src/Components/Products/Products.jsx
+++ frontend/src/Components/Products/Products.jsx
@@ -5,7 +5,7 @@ const Products = (props) => {
   return (
     <div className="iten">
       <p   className="product-flag">{props.flag}</p>
-      <img className="product-img" src={props.imgURL} alt={props.name}  />
+      <img className="product-img" src="./images/mouse.png" alt={props.name}/>
       <h2  className="product-name">{props.name}</h2>
       <h4  className="product-price">R$ {props.price}</h4>
       <p   className="product-installments">{props.installments}</p>
